0128b4398be8f0f6611670d74598bfd0fe8abe31
Justa <filjust@gmail.com>
2017-02-21 23:48:41 +0100

    Nowy projekt - komentarz

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name); // comment....
+console.log('Witaj, ' + name); // another comment...
+/* 
+var areaOfTriangle = 0,
+      userName = 'Piotr';
+*/
